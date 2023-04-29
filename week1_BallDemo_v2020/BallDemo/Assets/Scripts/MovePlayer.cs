be26147ca0ab6e07079094cb3f83ea661af31534
Soiiz <kchau6@ucsc.edu>
2023-04-29 02:08:38 -0700

    jump added

--- week1_BallDemo_v2020/BallDemo/Assets/Scripts/MovePlayer.cs
+++ week1_BallDemo_v2020/BallDemo/Assets/Scripts/MovePlayer.cs
@@ -3,50 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MovePlayer : MonoBehaviour {
-
+public class MovePlayer : MonoBehaviour
+{
+	public float JumpStrength;
 	public float speed;
 	public Text countText;
 
 	private Rigidbody rb;
 	private int count;
 
-
-	// Use this for initialization
-	void Start () 
+	private float WaitTime = 0;
+	void Start()
 	{
-		rb = GetComponent<Rigidbody> ();
+		rb = GetComponent<Rigidbody>();
 		count = 0;
-		setCountText ();
+		setCountText();
 	}
-	
+
 
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		bool jump = Input.GetKeyDown("space");
 
 		//countText.text = "(x,y,z): (" + rb.position.x.ToString() + ", " + rb.position.y.ToString() + ", " + rb.position.z.ToString() + ")";
+		WaitTime -= Time.deltaTime;
+		if ((jump == true) && (WaitTime < 0))
+		{
+			WaitTime = 1;
+			rb.AddForce(Vector3.up * JumpStrength);
+		}
 
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
-
-		rb.AddForce (movement*speed);
+		rb.AddForce(movement * speed);
 	}
 
-	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag ("Pick Up")) 
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
 		{
-			other.gameObject.SetActive (false);
+			other.gameObject.SetActive(false);
 			count = count + 1;
-			setCountText ();
+			setCountText();
 		}
 	}
 
 	void setCountText()
 	{
-		countText.text = "Count: " + count.ToString ();
+		countText.text = "Count: " + count.ToString();
 	}
-		
+
 }
